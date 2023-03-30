0598e9ea2cdd332d87ac91ac01cedf0279661ce6
Герасимович Вячеслав <slavacaas@yandex.ru>
2023-03-30 23:03:40 +0600

    исправил еще 2 ошибки pytest

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
@@ -8,3 +10,12 @@ urlpatterns = [
     path('about/', include('about.urls', namespace='about')),
     path('admin/', admin.site.urls),
 ]
+
+handler403 = 'core.views.permission_denied'
+handler404 = 'core.views.page_not_found'
+handler500 = 'core.views.server_error'
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
